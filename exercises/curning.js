e004eaab89d4b452f390fdd162ef2b94553058fa
Shubham Jain <pro.shubhamjain@gmail.com>
2024-01-14 12:00:51 +0530

    Update curning.js
    add diff way of implementation

--- exercises/curning.js
+++ exercises/curning.js
@@ -26,6 +26,43 @@ function closure() {
   }
 }
 
+//if the ask for exact format to apply function 
+function get() {
+  let value = 0;
+
+  const api = {
+    add: function (num) {
+      value += num;
+      return api;
+    },
+    sub: function (num) {
+      value -= num;
+      return api;
+    },
+    multiply: function (num) {
+      value *= num;
+      return api;
+    },
+    divided: function (num) {
+      if (num !== 0) {
+        value /= num;
+      } else {
+        console.error("Cannot divide by zero.");
+      }
+      return api;
+    },
+    getResult: function () {
+      return value;
+    },
+  };
+
+  return api;
+}
+
+// // Example usage:
+// const result = get().add(5).sub(2).multiply(10).divided(2).getResult();
+// console.log(result); // Output: 20
+
 
 module.exports = {
   calc,
